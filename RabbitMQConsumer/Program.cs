4885b1680bd79c67410ac3423abb4897df2e2b7a
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2024-03-23 10:43:14 +0300

    Add project files.

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: "fanoutexhange", type: ExchangeType.Fanout);
+
+            channel.QueueDeclare("QueueB", false, false, false);
+            channel.QueueBind("QueueB", "fanoutexhange", "");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, eventArgs) =>
+            {
+                var body = eventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine("Message received From second Consumer: " + message);
+            };
+            channel.BasicConsume(queue: "QueueB", autoAck: true, consumer: consumer);
+        }
+    }
+}
